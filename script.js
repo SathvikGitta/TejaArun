6756e8d1bdd14ef3a6e5120cc70a374d8f05cffa
SathvikGitta <sathvikgitta@gmail.com>
2023-12-27 03:06:16 +0530

    Created Section-02 && Section-03

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("hello world ");
+let btn_upwork_redirect = document.querySelector(".button-consultation");
+btn_upwork_redirect.style.cursor = "pointer";
+
+btn_upwork_redirect.addEventListener("click", () => {
+  window.open("https://www.upwork.com/freelancers/~0110b5735a167193bd");
+});
