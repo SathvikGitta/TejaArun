7368332c9c74cb24e3bd01192350c5bfb440f66f
SathvikGitta <sathvikgitta@gmail.com>
2023-12-30 19:12:16 +0530

    Build V 1.0.0

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ let btn_upwork_redirect = document.querySelector(".cta-button");
 btn_upwork_redirect.style.cursor = "pointer";
 
 btn_upwork_redirect.addEventListener("click", () => {
-  window.open("https://www.upwork.com/freelancers/~0110b5735a167193bd");
+  window.open("https://calendly.com/tejaarun/");
 });
 
 // Copy Notification Email
